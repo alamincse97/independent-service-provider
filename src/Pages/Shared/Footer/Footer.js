cec4463dddb93b2c5a22c3fd992c34c07afcf2dd
ImAlAmin <alamin.cse97@gmail.com>
2023-05-05 18:08:56 +0600

    added to footer

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className='bg-black m-0 py-4'>
+            <p className='footer-text text-white'>© 2022 Travel Guide. All rights reserved
+            <br />
+            Web build by Abu Naem
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
